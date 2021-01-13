b25f5f54becd41848db02adf133343b627d68a44
mayjinteoh <mayjin.teoh@gmail.com>
2021-01-13 20:45:40 +1300

    maxevent method added, plots added for events and stations

--- obspyInterface.py
+++ obspyInterface.py
@@ -36,6 +36,8 @@ class obspyInterface:
     self.endmonth = None
     self.startday = None
     self.endday = None
+    self.starthour = None
+    self.endhour = None
     self.minmag = None
     self.cat = None
     self.start = None
@@ -81,6 +83,17 @@ class obspyInterface:
     return self.startday
   def getendday(self):
     return self.endday  
+
+  def sethour(self, starthour, endhour = None):
+    self.starthour = int(starthour)
+    if endhour == None:
+      self.endhour = int(starthour)
+    else:
+      self.endhour = int(endhour)
+  def getstarthour(self):
+    return self.starthour
+  def getendhour(self):
+    return self.endhour 
 
   def setminmag(self, minmag):
     self.minmag = float(minmag)
@@ -152,13 +165,16 @@ class obspyInterface:
     self.cat = self.client.get_events(starttime = self.start, endtime = start.end, minmagnitude = self.minmag)
     return self.cat
  
-  def SearchByDate(self, year, month, day):
+  def SearchByDate(self, year, month, day, starthour, endhour, minmag):
     self.setyear(year)
     self.setmonth(month)
     self.setday(day)
-    self.start = UTCDateTime(self.startyr, self.startmonth, self.startday, 0, 0, 0)
-    self.end = UTCDateTime(self.startyr, self.endmonth, self.endday, 23, 59, 59)
+    self.sethour(starthour, endhour)
+    self.setminmag(minmag)
+    self.start = UTCDateTime(self.startyr, self.startmonth, self.startday, self.starthour, 0, 0)
+    self.end = UTCDateTime(self.startyr, self.endmonth, self.endday, self.endhour, 59, 59)
     self.cat = self.client.get_events(starttime = self.start, endtime = self.end, minmagnitude = self.minmag)
+    self.cat.plot(projection = "local")
     return self.cat
 
   def SearchByDatetoDate(self, startyr, endyr, startmonth, endmonth, startday, endday, minmag):
@@ -169,62 +185,63 @@ class obspyInterface:
     self.start = UTCDateTime(self.startyr, self.startmonth, self.startday, 0, 0, 0)
     self.end = UTCDateTime(self.endyr, self.endmonth, self.endday, 23, 59, 59)
     self.cat = self.client.get_events(starttime = self.start, endtime = self.end, minmagnitude = self.minmag)
+    self.cat.plot(projection = "local")
     return self.cat
-    #return self.cat.plot(projection = "local")
 
   def SearchByEventID(self, eventid):
     self.eventid = eventid
     self.cat = self.client.get_events(eventid = self.eventid)
     self.start = self.cat[0].origins[0].time
     self.end = self.start + 600
+    self.cat.plot(projection = "local")
     return self.cat
 
-  def maxevent(self): #event of maximum magnitude based on preferred magnitude type
+  def maxevent(self): #event of maximum magnitude based on preferred magnitude type  
     for ev in self.cat:
-      for m in range(len(ev.magnitudes)):
-        if 'uncertainty' in ev.magnitudes[m].mag_errors and ev.magnitudes[m].mag_errors['uncertainty'] != None and ev.magnitudes[m].resource_id == ev.preferred_magnitude_id: #preferred magnitude type taken from https://github.com/GeoNet/data-tutorials/blob/main/Seismic_Data/Python/GeoNet_FDSN_demo_event.ipynb > In [11]
-          #print('%s = %f +/- %f - Preferred magnitude' % (ev.magnitudes[m].magnitude_type, ev.magnitudes[m].mag, ev.magnitudes[m].mag_errors['uncertainty']))
-          if ev.magnitudes[m].mag + ev.magnitudes[m].mag_errors['uncertainty'] > self.maxmag:
-            self.maxmag = ev.magnitudes[m].mag + ev.magnitudes[m].mag_errors['uncertainty']
+        if len(self.cat) > 1:
+            for m in range(len(ev.magnitudes)):
+                if ev.magnitudes[m].magnitude_type == "M":
+                    if ev.magnitudes[m].mag > self.maxmag:
+                        self.maxmag = ev.magnitudes[m].mag
+                        self.maxev = ev
+        else:
             self.maxev = ev
     return self.maxev
 
   def stations(self, maxrad):
-    #origin = self.maxev.origins
+    self.maxevent()
     self.longitude = self.maxev.origins[0]['longitude']
     self.latitude = self.maxev.origins[0]['latitude']
     self.inventory = self.client.get_stations(latitude = float(self.latitude), longitude = float(self.longitude), maxradius = float(maxrad), starttime = self.start, endtime = self.end)
     #self.inventory = self.client.get_stations(latitude=-42.693,longitude=173.022,maxradius=0.5, starttime = "2016-11-13 11:05:00.000",endtime = "2016-11-14 11:00:00.000")
-    return(self.inventory)
+    self.inventory.plot(projection = "local")
+    return self.inventory
 
   def getwaveforms(self): 
     self.st = Stream()
-
     for network in self.inventory:
+        print(network)
         for station in network:
+            print(station)
             try:
                 self.st += self.client.get_waveforms(network.code, station.code, "*", "*",
                                           self.start, self.end, attach_response = True) #refer to link for arguments for get waveforms: https://docs.obspy.org/packages/autogen/obspy.clients.fdsn.client.Client.get_waveforms.html?highlight=get_waveforms#obspy.clients.fdsn.client.Client.get_waveforms
             except:
                 pass
-
+    self.st.plot()
     return self.st
-    #st.plot()
-
-  def getdata(self):
-    f = open("myfile.txt", "w")
-    for trace in self.st:
-      f.write(trace)
-    print(f.read)
+    
+  def gettrace(self):
+      self.getwaveforms()
+      for trace in self.st:
+          print(trace.data)
  
 # Unit testing     
 def main():
   obspyTest = obspyInterface('GEONET')
-  print(obspyTest.SearchByDate(2016, 11, 13))
-  print(obspyTest.maxevent())
-  print(obspyTest.stations(0.5))
+  print(obspyTest.SearchByDate(2020, 5, 24, 0, 23, 5))
+  print(obspyTest.stations(0.2))
   print(obspyTest.getwaveforms())
-  obspyTest.getdata()
-
+  #obspyTest.gettrace()
 if __name__ == "__main__":
     main()
